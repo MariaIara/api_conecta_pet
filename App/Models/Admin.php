0e1b61c5adb2418c5769314d91d7b0bdf8336c8b
Iara Braga <mariaiarasbraga@gmail.com>
2024-10-14 20:48:25 -0300

    admin model

--- App/Models/Admin.php
+++ App/Models/Admin.php
@@ -6,5 +6,16 @@ use Core\Model;
 
 class Admin extends Model
 {
-    protected $table;
+    protected $table = 'admin';
+
+    public function isLevelTwoAdmin($adminId)
+    {
+        $admin = $this->findBy('id', $adminId);
+        return $admin['nivel'] === 2;
+    }
+
+    public function verifyPassword($input_password, $stored_password)
+    {
+        return password_verify($input_password, $stored_password);
+    }
 }
