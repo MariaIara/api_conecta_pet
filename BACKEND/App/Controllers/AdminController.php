a8dffdf82acee11cf39aec289999aba977bea0c5
Iara Braga <mariaiarasbraga@gmail.com>
2024-10-25 15:15:46 -0300

    Correção do erro CORS

--- BACKEND/App/Controllers/AdminController.php
+++ BACKEND/App/Controllers/AdminController.php
@@ -50,6 +50,21 @@ class AdminController extends Controller
         return $this->response(201, ['success' => 'Novo administrador criado']);
     }
 
+    // Verifica Permissões
+    public function checkPermissions()
+    {
+        // $authData = $this->checkAuth();
+
+        // if (!$authData) {
+        //     return false;
+        // }
+
+        // return $this->admin_model->isLevelTwoAdmin($authData->admin_id);
+
+        return true;
+    }
+
+
     public function excluir($id){
         $permission = $this->checkPermissions();
 
@@ -83,19 +98,6 @@ class AdminController extends Controller
         return $this->response(200, ['success' => 'Alteração admnistrativa realizada com sucesso!']);
     }
 
-        // Verifica Permissões
-    public function checkPermissions()
-    {
-        // $authData = $this->checkAuth();
-
-        // if (!$authData) {
-        //     return false;
-        // }
-
-        // return $this->admin_model->isLevelTwoAdmin($authData->admin_id);
-
-        return true;
-    }
 
     // Fazer Login De Admin
     public function login()
--- BACKEND/Core/Push.php
+++ BACKEND/Core/Push.php
@@ -29,6 +29,11 @@ class Push
         header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE");
         header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
+        if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+            http_response_code(200);
+            exit;
+        }
+        
         Router::run();
     }
 }
