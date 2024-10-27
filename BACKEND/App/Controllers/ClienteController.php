c315cbce3ad12ca5b5bce9c1253008b215d10ece
Iara Braga <mariaiarasbraga@gmail.com>
2024-10-27 19:01:05 -0300

    Mudança na foreign key

--- BACKEND/App/Controllers/ClienteController.php
+++ BACKEND/App/Controllers/ClienteController.php
@@ -9,7 +9,8 @@ class ClienteController extends Controller
 {
     protected $cliente_model;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->cliente_model = new Cliente();
     }
 
@@ -19,34 +20,43 @@ class ClienteController extends Controller
         return $this->response(200, $cliente);
     }
 
-    public function show($id){
+    public function show($id)
+    {
         $cliente = $this->cliente_model->find($id);
 
-        if(!$cliente){
+        if (!$cliente) {
             return $this->response(404, ['error' => 'O cliente não existe!']);
         }
 
         return $this->response(200, $cliente);
     }
 
-    public function store(){
-        $this->cliente_model->save($this->getRequestBody());
-        return $this->response(200, ['success' => 'Cliente criado com sucesso!']);
-        // $cliente_model = new Cliente();
-        // $cliente = $this->getRequestBody();
-        // $cliente_model->save($cliente);
-        // return $this->response(200, ['success' => 'Cliente criado com sucesso!']);
+    public function store()
+    {
+        $cliente = $this->getRequestBody(); // Cliente retornado do formulário no formato de um array associativo
+
+        $cliente_existe = $this->cliente_model->findBy('cpf', $cliente['cpf']); // Procura um cliente no banco com o campo cpf com o valor do segundo parâmetro
+
+        if ($cliente_existe) { // Se já existir um cliente com esse cpf:
+            return $this->response(409, ['error' => 'Já existe um cliente com o CPF informado']);
+        }
+
+        $this->cliente_model->save($cliente); // Adiciona cliente no banco
+
+        return $this->response(200, ['success' => 'Cliente criado com sucesso!']); // Mensagem de sucesso :)
     }
 
-    public function update($id){
+    public function update($id)
+    {
         $this->cliente_model->update($id, $this->getRequestBody());
         return $this->response(200, ['success' => 'Alteração realizada com sucesso!']);
     }
 
-    public function delete($id){
+    public function destroy($id)
+    {
         $cliente = $this->cliente_model->find($id);
 
-        if(!$cliente){
+        if (!$cliente) {
             return $this->response(404, ['error' => 'Este cliente não está cadastrado em nossa base de dados']);
         }
 
@@ -54,4 +64,3 @@ class ClienteController extends Controller
         return $this->response(200, ['success' => 'Cliente excluído com sucesso!']);
     }
 }
-
--- BACKEND/App/Controllers/PetController.php
+++ BACKEND/App/Controllers/PetController.php
@@ -4,13 +4,17 @@ namespace App\Controllers;
 
 use Core\Controller;
 use App\Models\Pet;
+use App\Models\Cliente;
 
 class PetController extends Controller
 {
     protected $pet_model;
+    protected $cliente_model;
 
-    public function __construct(){
+    public function __construct()
+    {
         $this->pet_model = new Pet();
+        $this->cliente_model = new Cliente();
     }
 
     public function index() //Retorna todos os registros de pets
@@ -20,30 +24,48 @@ class PetController extends Controller
     }
 
     public function show($id) //Retorna um registro de pet específico por id
-    {  
+    {
         $pet = $this->pet_model->find($id);
 
-        if(!$pet){
+        if (!$pet) {
             $this->response(404, ['Message' => 'Pet não encontrado']);
         }
 
         $this->response(200, $pet);
     }
 
-    public function store(){
-        $this->pet_model->save($this->getRequestBody());
-        return $this->response(201, ['sucess' => 'Novo pet criado!']);
+    public function store()
+    {
+        $pet = $this->getRequestBody(); // Pet retornado do formulario no formato:
+        // [
+        //  nome => 'penina',
+        //  tipo => 'cachorro',
+        //  ...
+        //  cliente_cpf => '00000000000'
+        // ]
+
+        $cliente_existe = $this->cliente_model->findBy('cpf', $pet['key_cliente_cpf']); // findBy (função do framework que procura um registro no banco na tabela da model selecionada (cliente) onde a coluna do primeiro parâmetro que tenha o valor do segundo parâmetro) 
+
+        if (!$cliente_existe) { // Caso o cliente com o CPF informado não exista:
+            return $this->response(404, ['error' => 'Não foi possível encontrar um cliente com o CPF informado']);
+        }
+
+        $this->pet_model->save($pet); // Salva o pet no banco
+
+        return $this->response(201, ['sucess' => 'Novo pet criado!']); // Mensagem de sucesso :)
     }
 
-    public function update($id){
+    public function update($id)
+    {
         $this->pet_model->update($id, $this->getRequestBody());
         return $this->response(200, ['sucess' => 'Alteração realizada com sucesso!']);
     }
 
-    public function delete($id){
+    public function destroy($id)
+    {
         $pet = $this->pet_model->find($id);
 
-        if(!$pet){
+        if (!$pet) {
             return $this->response(404, ['error' => 'O pet não existe!']);
         }
 
