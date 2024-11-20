6aadd6525c01a145eed769ef3cc1dd109a93f657
Gustavo Leite <qustavoleite@gmail.com>
2024-11-20 00:35:10 -0300

    bug do tipo de animla resolvido

--- FRONTEND/js/pet_registre.js
+++ FRONTEND/js/pet_registre.js
@@ -1,11 +1,17 @@
 document.getElementById('petForm').addEventListener('submit', async (event) => {
   event.preventDefault()
 
+  const tipoAnimal = {
+    Cachorro: 0,
+    Gato: 1,
+  }
+
   const petData = {
     nome: document.getElementById('nome').value,
     raca: document.getElementById('raca').value,
     sexo: document.querySelector('input[name="sexo"]:checked').value,
-    animal: document.querySelector('input[name="tipo"]:checked').value,
+    animal:
+      tipoAnimal[document.querySelector('input[name="animal"]:checked').value], 
     microchip: document.getElementById('microchip').value,
     cliente_cpf: document.getElementById('cpf').value.replace(/\D/g, ''),
   }
@@ -35,7 +41,7 @@ document.getElementById('petForm').addEventListener('submit', async (event) => {
 })
 
 function formatCPF(cpfField) {
-  let cpf = cpfField.value.replace(/\D/g, '') 
+  let cpf = cpfField.value.replace(/\D/g, '')
   cpf = cpf.slice(0, 11)
   cpfField.value = cpf
     .replace(/(\d{3})(\d)/, '$1.$2')
