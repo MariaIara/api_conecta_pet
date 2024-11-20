22769e31f3f5135c9566e004c811ca294820f49b
Gustavo Leite <qustavoleite@gmail.com>
2024-11-20 00:34:09 -0300

    mais campos de cadastro, função de mostrar senha e formatar cpf

--- FRONTEND/js/create_adm.js
+++ FRONTEND/js/create_adm.js
@@ -1,60 +1,24 @@
-// document
-//   .getElementById('adminForm')
-//   .addEventListener('submit', async function (e) {
-//     e.preventDefault()
-
-//     const email = document.getElementById('email').value
-//     const senha = document.getElementById('senha').value
-//     const tipo = document.querySelector('input[name="adminType"]:checked').value
-
-//     const novoAdmin = {
-//       email: email,
-//       senha: senha,
-//       tipo: parseInt(tipo),
-//     }
-
-//     try {
-//       const response = await fetch('http://localhost:8080/admin/cadastro', {
-//         method: 'POST',
-//         headers: {
-//           'Content-Type': 'application/json',
-//         },
-//         body: JSON.stringify(novoAdmin),
-//       })
-
-//       const result = await response.json()
-
-//       if (response.ok) {
-//         alert('Novo administrador criado com sucesso!')
-//         console.log('Novo administrador:', result)
-//         window.location.href = 'administrative_panel.html'
-//       } else {
-//         alert(result.error || 'Erro ao criar o administrador.')
-//         console.error('Erro:', result.error)
-//       }
-//     } catch (error) {
-//       console.error('Erro ao conectar com o servidor:', error)
-//       alert('Erro ao conectar com o servidor.')
-//     }
-//   })
-
 document
   .getElementById('adminForm')
   .addEventListener('submit', async function (e) {
     e.preventDefault()
 
+    const nome = document.getElementById('nome').value.trim()
+    const cpf = document.getElementById('cpf').value.replace(/\D/g, '').trim()
     const email = document.getElementById('email').value.trim()
     const senha = document.getElementById('senha').value.trim()
     const tipo = document.querySelector(
       'input[name="adminType"]:checked'
     )?.value
 
-    if (!email || !senha || !tipo) {
+    if (!nome || !cpf || !email || !senha || !tipo) {
       alert('Por favor, preencha todos os campos.')
       return
     }
 
     const novoAdmin = {
+      nome,
+      cpf,
       email,
       senha,
       tipo: parseInt(tipo),
@@ -93,3 +57,17 @@ document
       alert('Erro ao conectar com o servidor.')
     }
   })
+
+function togglePassword() {
+  const passwordField = document.getElementById('senha')
+  passwordField.type = passwordField.type === 'password' ? 'text' : 'password'
+}
+
+function formatCPF(cpfField) {
+  let cpf = cpfField.value.replace(/\D/g, '')
+  cpf = cpf.slice(0, 11)
+  cpfField.value = cpf
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d)/, '$1.$2')
+    .replace(/(\d{3})(\d{1,2})$/, '$1-$2')
+}
